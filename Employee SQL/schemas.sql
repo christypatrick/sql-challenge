fd6b5491b33d6cef10ce82cf21bea7672849d352
Christy Patrick <christy06p@msn.com>
2020-10-04 20:36:11 -0500

    added tables and loaded all csv files

--- Employee SQL/schemas.sql
+++ Employee SQL/schemas.sql
@@ -21,3 +21,43 @@ CREATE TABLE IF NOT EXISTS employees (
 
 -- View table columns and datatypes
 SELECT * FROM employees;
+
+-- Create new table - salaries
+CREATE TABLE IF NOT EXISTS salaries (
+	emp_no int Primary Key,
+	salary int
+);
+
+-- View table columns and datatypes
+SELECT * FROM salaries;
+
+-- Create new table - departments
+CREATE TABLE IF NOT EXISTS departments (
+	dept_no varchar(50) Primary Key,
+	dept_name varchar(50)
+);
+
+-- View table columns and datatypes
+SELECT * FROM departments;
+
+-- Create new table - dept_emp
+CREATE TABLE IF NOT EXISTS dept_emp (
+	emp_no int,
+	dept_no varchar(50),
+	Foreign Key (emp_no) References employees(emp_no),
+	Foreign Key (dept_no) References departments(dept_no)
+);
+
+-- View table columns and datatypes
+SELECT * FROM dept_emp;
+
+-- Create new table - dept_manager
+CREATE TABLE IF NOT EXISTS dept_manager (
+	dept_no varchar(50),
+	emp_no int,
+	Foreign Key (dept_no) References departments(dept_no),
+	Foreign Key (emp_no) References employees(emp_no)
+);
+
+-- View table columns and datatypes
+SELECT * FROM dept_manager;
